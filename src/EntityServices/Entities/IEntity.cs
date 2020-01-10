e57a121ced2502f81a8c3ab24f4b81a51d211ffe
Casey Copeland <casey@copeland.tech>
2020-01-10 11:32:46 -0600

    Added interface contracts for Entity Services

---
+++ src/EntityServices/Entities/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebApi_EntityFrameworkCore_Example.EntityServices.Entities
+{
+    public interface IEntity
+    {
+        Guid Guid { get; }
+    }
+}
---
+++ src/EntityServices/Repository/IEntityService.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using WebApi_EntityFrameworkCore_Example.EntityServices.Entities;
+
+namespace WebApi_EntityFrameworkCore_Example.EntityServices.Repository
+{
+    public interface IEntityService<TEntity> where TEntity : class, IEntity
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> GetById(int id);
+
+        Task Create(TEntity entity);
+
+        Task Update(int id, TEntity entity);
+
+        Task Delete(int id);
+    }
+}
